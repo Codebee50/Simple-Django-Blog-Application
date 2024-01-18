201b9be4bebc8424dc1d0cb56d41566ea647b77e
Code bee <codebee345@outlook.com>
2024-01-18 20:07:37 +0100

    added share post by email feature

--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,48 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.http import Http404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 # Create your views here.
-def post_list(request):
 
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id= post_id, status = Post.Status.PUBLISHED)
+    sent = False
+    if request.method == 'POST':
+        #fORM WAS SUBMITTED
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            #send email
+        post_url = request.build_absolute_uri(post.get_absolute_url())
+
+        subject = f"{cd['name']} recommends you read " \
+                f"{post.title}"
+        
+        message = f"Read {post.title} at {post_url}\n\n"\
+        f"{cd['name']}\'s comments: {cd['comments']}"
+
+        send_mail(subject, message, 'onuhudoudo@gmail.com', [cd['to']])
+        sent =True
+        
+    else:
+        form = EmailPostForm()
+    
+    return render(request, 'post/share.html', {'post': post, 'form': form, 'sent': sent})
+
+class PostListView(ListView):
+    """
+    Alternative post list view
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'post/list.html'
+
+
+def post_list(request):
     post_list = Post.published.all()
 
     paginator = Paginator(post_list, 3)
@@ -17,8 +55,6 @@ def post_list(request):
         posts = paginator.page(paginator.num_pages) #paginator.num_pages returns the total number of pages available 
     except PageNotAnInteger:
         posts = paginator.page(1)
-
-    
     return render(request, 'post/list.html', {'posts': posts})
 
 def post_detail(request, year, month, day, post):
